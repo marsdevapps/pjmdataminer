a9e5981159ad9f4d64ab4ed175a2e08470423cff
John Childress <john@mars-dev.com>
2017-08-08 22:21:26 -0500

    Stub for RT LMP Prices

---
+++ src/main/kotlin/com/marsdev/pjm/models/LMPRealTime.kt
@@ -0,0 +1,8 @@
+package com.marsdev.pjm.models
+
+/**
+ * https://dataminer.pjm.com/dataminer/rest/public/api/markets/realtime/lmp/daily?startDate=12/1/2016&endDate=12/4/2016&pnodeList:[51288]
+ */
+class LMPRealTime {
+
+}
