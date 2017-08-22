5ca6d2dda2ebaa9481bdc4a4f8d72656d33966c5
John Childress <john@mars-dev.com>
2017-08-22 08:53:01 -0500

    Initial Layout/Design

--- src/main/kotlin/com/marsdev/pjm/app/PJMDataMinerApp.kt
+++ src/main/kotlin/com/marsdev/pjm/app/PJMDataMinerApp.kt
@@ -3,4 +3,8 @@ package com.marsdev.pjm.app
 import com.marsdev.pjm.views.MainView
 import tornadofx.*
 
-class PJMDataMinerApp : App(MainView::class, Styles::class)
+class PJMDataMinerApp : App(MainView::class, Styles::class) {
+    init {
+        setStageIcon(resources.image("app-icon.png"))
+    }
+}
--- src/main/kotlin/com/marsdev/pjm/views/MainView.kt
+++ src/main/kotlin/com/marsdev/pjm/views/MainView.kt
@@ -5,7 +5,7 @@ import tornadofx.*
 class MainView : View("PJM Data Miner 2.0") {
 
     override val root = borderpane {
-        prefWidth = 800.0
+        prefWidth = 1200.0
         prefHeight = 800.0
         center(ContentView::class)
         left(NavMenuView::class)
